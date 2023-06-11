be1ef1362cabf7cbe5832e084bd1ba72c1cf3b68
Kyaw Thu <evanch98@gmail.com>
2023-06-11 23:09:48 +0800

    add a new component Header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,9 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+  return (
+    <div class="flex justify-between items-center w-full h-20">
+      
+    </div>
+  );
+});
