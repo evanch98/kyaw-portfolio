adb50fbd6757b79c11e2704eeee060c3878a5341
Kyaw Thu <evanch98@gmail.com>
2023-06-11 23:01:55 +0800

    add metadata

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,11 +10,11 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: 'Kyaw | Front-End Developer',
   meta: [
     {
-      name: 'description',
-      content: 'Qwik site description',
+      name: 'Kyaw Thu\'s Portfolio Website',
+      content: 'This website is designed and built by Kyaw Thu. It is built by using Qwik, Qwik City, TypeScript, Motion One, and Tailwind CSS.',
     },
   ],
 };
