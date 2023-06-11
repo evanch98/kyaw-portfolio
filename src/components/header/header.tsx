879070629e9fa86b79a5b3d3cbf47f57eb4da552
Kyaw Thu <evanch98@gmail.com>
2023-06-11 23:13:55 +0800

    update the Header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,17 @@
 import { component$ } from "@builder.io/qwik";
+import { Image } from "@unpic/qwik";
 
 export default component$(() => {
   return (
     <div class="flex justify-between items-center w-full h-20">
-      
+      <Image 
+        src="/assets/logo.png"
+        layout="constrained"
+        width={64}
+        height={64}
+        alt="A logo image"
+      />
+      <p>evanch98@gmail.com</p>
     </div>
   );
 });
