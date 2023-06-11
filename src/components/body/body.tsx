1f00558479c85cfff79be100f936326ecb06106b
Kyaw Thu <evanch98@gmail.com>
2023-06-12 01:42:31 +0800

    add the MainCard component

--- src/components/body/body.tsx
+++ src/components/body/body.tsx
@@ -1,10 +1,12 @@
 import { component$ } from "@builder.io/qwik";
 import IntroCard from "../intro-card/intro-card";
+import MainCard from "../main-card/main-card";
 
 export default component$(() => {
   return (
     <div class="flex justify-between items-center w-full text-white p-5">
       <IntroCard />
+      <MainCard />
     </div>
   );
 });
