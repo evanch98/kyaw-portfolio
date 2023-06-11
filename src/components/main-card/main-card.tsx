be97a57376651f0145f50d4b48e89a71047cfbdf
Kyaw Thu <evanch98@gmail.com>
2023-06-12 01:45:24 +0800

    update the MainCard component

--- src/components/main-card/main-card.tsx
+++ src/components/main-card/main-card.tsx
@@ -2,7 +2,7 @@ import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
-    <div class="flex-2">
+    <div class="flex-1 w-full h-full flex flex-col bg-[#013329] rounded-md overflow-hidden">
       <div>Main Card</div>
     </div>
   );
