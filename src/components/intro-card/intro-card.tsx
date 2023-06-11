9d7c354b1d5435dbe82f8cb0228a600f3354e500
Kyaw Thu <evanch98@gmail.com>
2023-06-11 23:55:20 +0800

    update the IntroCard component

--- src/components/intro-card/intro-card.tsx
+++ src/components/intro-card/intro-card.tsx
@@ -1,7 +1,23 @@
 import { component$ } from "@builder.io/qwik";
+import { Image } from "@unpic/qwik";
 
 export default component$(() => {
   return (
-    <div class="flex-1">IntroCard</div>
+    <div class="flex-1 w-full h-full flex flex-col bg-[#013329] rounded-md">
+      <section class="flex flex-col p-5 items-center justify-center">
+        <Image
+          src="/assets/avatar.png"
+          layout="constrained"
+          width={247}
+          height={247}
+          alt="An avatar image"
+        />
+        <h1 class="tracking-widest text-4xl text-[#CC8B65]">Kyaw Thu</h1>
+        <p class="text-lg text-[#E3DCD2] mt-5">
+          I am an aspiring front-end web developer.
+        </p>
+        <div class="flex items-center justify-evenly"></div>
+      </section>
+    </div>
   );
 });
