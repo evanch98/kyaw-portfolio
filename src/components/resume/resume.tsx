f939ab29cd1f2082520ffd73caa53368379f080d
Kyaw Thu <evanch98@gmail.com>
2023-06-12 16:50:31 +0800

    update the Resume component

--- src/components/resume/resume.tsx
+++ src/components/resume/resume.tsx
@@ -1,7 +1,28 @@
 import { component$ } from "@builder.io/qwik";
+import Title from "../common/title/title";
+import { BsMortarboard } from "@qwikest/icons/bootstrap";
 
 export default component$(() => {
   return (
-    <div>Resume</div>
+    <div class="flex flex-col w-full pr-5 text-[#E3DCD2]">
+      <Title title="Resume" />
+      <div class="flex items-start justify-between mt-5">
+        <div class="w-full flex flex-col items-center justify-center text-lg">
+          <div class="flex justify-center items-center space-x-2">
+            <h1 class="text-xl font-bold">Degree</h1>
+            <BsMortarboard class="w-[24px] h-auto" />
+          </div>
+          <div class="flex flex-col items-center justify-center mt-5">
+            <p>Bachelor of Science in Computer Science</p>
+            <p>University of the People, CA</p>
+            <p>2021-Present</p>
+          </div>
+        </div>
+        <div class="h-full bg-[#E3DCD2] w-0.5"></div>
+        <div class="w-full flex flex-col items-center justify-center">
+          <h1 class="text-xl font-bold">Certificates</h1>
+        </div>
+      </div>
+    </div>
   );
 });
