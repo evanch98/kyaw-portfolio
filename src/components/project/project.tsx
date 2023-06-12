73af17c9b32e90d2a657b52c68ed6ab9b88e800e
Kyaw Thu <evanch98@gmail.com>
2023-06-12 23:11:02 +0800

    update the Project component

--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -1,14 +1,38 @@
 import { component$ } from "@builder.io/qwik";
 import Title from "../common/title/title";
-import HorizontalSeparator from "../common/separators/horizontal-separator/horizontal-separator";
+import { Image } from "@unpic/qwik";
+import VerticalSeparator from "../common/separators/vertical-separator/vertical-separator";
 
 export default component$(() => {
   return (
-    <div class="flex flex-col">
+    <div class="flex flex-col w-full text-[#E3DCD2]">
       <Title title="Projects" />
       <div class="flex flex-col mt-5 space-y-6">
-        <div class="flex items-start justify-between"></div>
-        <HorizontalSeparator />
+        <div class="flex items-start justify-between">
+          <div class="w-full flex flex-col items-center justify-center">
+            <div class="relative">
+              <Image
+                src="/projects/airbnb.png"
+                width={300}
+                height={148}
+                alt="airbnb"
+                layout="constrained"
+              />
+            </div>
+          </div>
+          <VerticalSeparator />
+          <div class="w-full flex flex-col items-center justify-center">
+            <div class="relative">
+              <Image
+                src="/projects/share_prompts.png"
+                width={300}
+                height={148}
+                alt="airbnb"
+                layout="constrained"
+              />
+            </div>
+          </div>
+        </div>
         <div class="flex items-start justify-between"></div>
       </div>
     </div>
