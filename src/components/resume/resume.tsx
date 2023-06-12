1176b1b349347b730ebd38f3870cd11d5ce0e4e2
Kyaw Thu <evanch98@gmail.com>
2023-06-12 17:01:19 +0800

    update the Resume component

--- src/components/resume/resume.tsx
+++ src/components/resume/resume.tsx
@@ -1,13 +1,13 @@
 import { component$ } from "@builder.io/qwik";
 import Title from "../common/title/title";
-import { BsMortarboard } from "@qwikest/icons/bootstrap";
+import { BsMortarboard, BsPatchCheck } from "@qwikest/icons/bootstrap";
 
 export default component$(() => {
   return (
     <div class="flex flex-col w-full pr-5 text-[#E3DCD2]">
       <Title title="Resume" />
       <div class="flex items-start justify-between mt-5">
-        <section class="w-full flex flex-col items-center justify-center text-lg">
+        <section class="w-full flex flex-col items-center justify-center">
           <div class="flex justify-center items-center space-x-2">
             <h1 class="text-xl font-bold">Degree</h1>
             <BsMortarboard class="w-[24px] h-auto" />
@@ -20,9 +20,28 @@ export default component$(() => {
         </section>
         <div class="h-full bg-[#E3DCD2] w-0.5"></div>
         <section class="w-full flex flex-col items-center justify-center">
-          <h1 class="text-xl font-bold">Certificates</h1>
+          <div class="flex justify-center items-center space-x-2">
+            <h1 class="text-xl font-bold">Certificates</h1>
+            <BsPatchCheck class="w-[24px] h-auto" />
+          </div>
+          <div class="flex flex-col items-center justify-center mt-5">
+            <p>Meta Front-End Developer Professional Certificate</p>
+            <p>Meta and Coursera</p>
+            <p>June 2023-Present</p>
+          </div>
+          <div class="flex flex-col items-center justify-center mt-5">
+            <p>Meta Android Developer Professional Certificate</p>
+            <p>Meta and Coursera</p>
+            <p>September 2022-April 2023</p>
+          </div>
+          <div class="flex flex-col items-center justify-center mt-5">
+            <p>IBM Data Analyst Professional Certificate</p>
+            <p>IBM and Coursera</p>
+            <p>July 2021-August 2021</p>
+          </div>
         </section>
       </div>
+      <hr class="w-full border-solid border-1 border-[#E3DCD2] my-5" />
     </div>
   );
 });
