b3a5394722fc6e2d4eb2b57b6e44798212e402a3
Kyaw Thu <evanch98@gmail.com>
2023-06-12 19:42:12 +0800

    update the Project component

--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -1,10 +1,16 @@
 import { component$ } from "@builder.io/qwik";
 import Title from "../common/title/title";
+import HorizontalSeparator from "../common/separators/horizontal-separator/horizontal-separator";
 
 export default component$(() => {
   return (
     <div class="flex flex-col">
       <Title title="Projects" />
+      <div class="flex flex-col mt-5 space-y-6">
+        <div class="flex items-start justify-between"></div>
+        <HorizontalSeparator />
+        <div class="flex items-start justify-between"></div>
+      </div>
     </div>
   );
 });
