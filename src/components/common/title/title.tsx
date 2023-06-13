b2fee75278760c3ee08dd963def22a18a4aa9d37
Kyaw Thu <evanch98@gmail.com>
2023-06-13 12:01:30 +0800

    update the Title component

--- src/components/common/title/title.tsx
+++ src/components/common/title/title.tsx
@@ -6,7 +6,7 @@ interface TitleProps {
 
 export default component$((props: TitleProps) => {
   return (
-    <p class="text-3xl first-letter:font-bold first-letter:underline first-letter:text-[#CC8B65]">
+    <p class="text-3xl first-letter:font-bold first-letter:underline first-letter:text-[#F7CE3E]">
       {props.title}
     </p>
   );
