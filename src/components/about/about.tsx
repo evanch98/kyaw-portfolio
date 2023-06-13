66cf5a2fcf34ebe1b5d792dce4deb1ab8321725b
Kyaw Thu <evanch98@gmail.com>
2023-06-14 00:40:02 +0800

    add animations for the About component

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useVisibleTask$ } from "@builder.io/qwik";
 import Title from "../common/title/title";
 import {
   IoFootballOutline,
@@ -8,13 +8,21 @@ import {
 } from "@qwikest/icons/ionicons";
 import VerticalSeparator from "../common/separators/vertical-separator/vertical-separator";
 import HorizontalSeparator from "../common/separators/horizontal-separator/horizontal-separator";
+import { animate, spring, stagger } from "motion";
 
 export default component$(() => {
+  useVisibleTask$(() => {
+    animate(
+      ".para",
+      { x: [-1000, 0] },
+      { easing: spring({ velocity: 50, stiffness: 50 }), delay: stagger(0.15) }
+    );
+  });
   return (
     <div class="flex flex-col">
       <Title title="About" />
       <div class="flex flex-col text-[#C5C1C0] text-lg mt-5 space-y-5">
-        <p>
+        <p class="para">
           I have spent 7 years studying medicine at the University of Medicine
           1, Yangon. Due to several reasons, including pandemic, I had to drop
           out of the university. For as long as I can remember, I have always
@@ -22,7 +30,7 @@ export default component$(() => {
           learning programming with Python, which made me love programming even
           more.
         </p>
-        <p>
+        <p class="para">
           After exploring several career options, I was quickly drawn to
           front-end web development. I started learning HTML and CSS to build
           simple websites. Fascinated with how intricate web development can be,
@@ -31,7 +39,7 @@ export default component$(() => {
           spending my time building projects with React JS, Next JS, Tailwind
           CSS, and MongoDB and learning new technologies.
         </p>
-        <p>
+        <p class="para">
           Apart from coding, I enjoy swimming. My favorite movie and TV show
           genres are Sci-fi and Sitcom. My favorite sport is soccer. My favorite
           team is Liverpool FC based in Liverpool, England. I also watch NBA
@@ -40,7 +48,7 @@ export default component$(() => {
       </div>
       <HorizontalSeparator />
       <Title title="Fun Facts" />
-      <div class="flex items-center justify-between mt-5 pb-2">
+      <div class="fun flex items-center justify-between mt-5 pb-2">
         <div class="flex flex-col items-center justify-center">
           <IoFootballOutline class="w-[28px] h-auto mb-2" />
           <h1 class="text-lg font-semibold">Favorite Sport</h1>
