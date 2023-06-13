c3d2baf04a6d25ce74310d245dcc13a7ba5a7bd2
Kyaw Thu <evanch98@gmail.com>
2023-06-13 21:21:23 +0800

    update the projects.ts file

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -7,3 +7,56 @@ export const portfolioWebsite = {
   tech: ["Qwik", "Qwik City", "TypeScript", "Tailwind CSS", "Motion One"],
   desc: ["Simple portfolio website"],
 };
+
+export const project1 = {
+  title: "Airbnb Clone",
+  sourceSrc: "https://github.com/evanch98/airbnb_clone_react",
+  liveSrc: "https://rent-house-react-hopf81d0y-evanch98.vercel.app/",
+  date: "5/8/2023",
+  category: "Hobby project",
+  tech: [
+    "Next JS",
+    "NextAuth",
+    "React",
+    "TypeScript",
+    "Tailwind CSS",
+    "Prisma",
+    "MongoDB",
+    "Cloudinary",
+  ],
+  desc: [
+    "Core Airbnb features",
+    "Email login, GitHub login, and Google login",
+    "MongoDB for data persistence",
+    "NextAuth authentication",
+    "Next JS 13 App Router",
+    "Users can view listings listed by other users",
+    "Users can make reservations based on their preferences",
+    "Users can list their properties and upload their properties image",
+  ],
+};
+
+export const project2 = {
+  title: "Share Prompts",
+  sourceSrc: "https://github.com/evanch98/share-prompts-nextjs",
+  liveSrc: "https://share-prompts-taupe.vercel.app/",
+  date: "5/25/2023",
+  category: "Hobby project",
+  tech: [
+    "Next JS",
+    "NextAuth",
+    "React",
+    "JavaScript",
+    "Tailwind CSS",
+    "MongoDB",
+  ],
+  desc: [
+    "Google login",
+    "MongoDB for data persistence",
+    "NextAuth authentication",
+    "Users can share their favorite prompts they used for AI-powered tools such as ChatGPT",
+    "Users can see prompts shared by other users",
+    "Users can copy the prompts",
+    "Users can edit their prompts",
+  ],
+};
