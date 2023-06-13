4858435a3bc60ca0f8d6ea97fad3d312a3e81085
Kyaw Thu <evanch98@gmail.com>
2023-06-13 17:30:50 +0800

    update the Modal component

--- src/components/common/modal/modal.tsx
+++ src/components/common/modal/modal.tsx
@@ -2,6 +2,6 @@ import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
-    <div>Modal</div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2">Modal</div>
   );
 });
