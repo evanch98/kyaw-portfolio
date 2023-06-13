e749709844a85b849208a7474c8363270c558b7a
Kyaw Thu <evanch98@gmail.com>
2023-06-14 01:11:14 +0800

    update the Uopeople component

--- src/components/uopeople/uopeople.tsx
+++ src/components/uopeople/uopeople.tsx
@@ -2,6 +2,12 @@ import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
-    <div>UoPeople</div>
+    <div class="flex flex-col w-[640px]">
+      <div class="flex flex-col items-start text-base">
+        <p>Degree --- Bachelor of Science in Computer Science</p>
+        <p>University --- University of the People, Pasadena, CA</p>
+        <p>Date --- September 2021 - Present</p>
+      </div>
+    </div>
   );
 });
