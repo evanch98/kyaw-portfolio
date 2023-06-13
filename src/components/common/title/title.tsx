32dcf6b9a0d16f0ab3ad20fd8ba0d28df9cfaeb2
Kyaw Thu <evanch98@gmail.com>
2023-06-14 00:42:20 +0800

    update the Title component

--- src/components/common/title/title.tsx
+++ src/components/common/title/title.tsx
@@ -1,12 +1,16 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useVisibleTask$ } from "@builder.io/qwik";
+import { animate } from "motion";
 
 interface TitleProps {
   title: string;
 }
 
 export default component$((props: TitleProps) => {
+  useVisibleTask$(() => {
+    animate(".title", { opacity: [0, 1] }, { easing: "ease-in", duration: 1 });
+  });
   return (
-    <p class="text-3xl first-letter:font-bold first-letter:underline first-letter:text-[#F7CE3E]">
+    <p class="title text-3xl first-letter:font-bold first-letter:underline first-letter:text-[#F7CE3E]">
       {props.title}
     </p>
   );
