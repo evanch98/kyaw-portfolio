ed2fa19128fd65d8271b031e10c8a1d14a6841ea
Kyaw Thu <evanch98@gmail.com>
2023-06-13 19:45:18 +0800

    add a new object portfolioWebsite

---
+++ src/utils/projects.ts
@@ -0,0 +1,9 @@
+export const portfolioWebsite = {
+  title: "Portfolio Website",
+  sourceSrc: "https://github.com/evanch98/kyaw-portfolio",
+  liveSrc: "/",
+  date: "6/12/2023",
+  category: "Hobby project",
+  tech: ["Qwik", "Qwik City", "TypeScript", "Tailwind CSS", "Motion One"],
+  desc: ["Simple portfolio website"],
+};
