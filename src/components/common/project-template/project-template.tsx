3c62add207b240fda5b938fe29eafd629429cbaf
Kyaw Thu <evanch98@gmail.com>
2023-06-13 19:39:31 +0800

    update the ProjectTemplate component

--- src/components/common/project-template/project-template.tsx
+++ src/components/common/project-template/project-template.tsx
@@ -1,27 +1,49 @@
 import { component$ } from "@builder.io/qwik";
 import HorizontalSeparator from "../separators/horizontal-separator/horizontal-separator";
-import { BsCodeSlash, BsGithub, BsPostcard, BsReceipt } from "@qwikest/icons/bootstrap";
+import {
+  BsCodeSlash,
+  BsGithub,
+  BsPostcard,
+  BsReceipt,
+} from "@qwikest/icons/bootstrap";
 
-export default component$(() => {
+interface ProjectTemplateProps {
+  sourceSrc: string;
+  liveSrc: string;
+  date: string;
+  category: string;
+  tech: Array<string>;
+  desc: Array<string>;
+}
+
+export default component$((props: ProjectTemplateProps) => {
   return (
     <div class="flex flex-col w-[640px]">
       <div class="flex flex-col items-start space-y-5">
         <div class="flex items-center justify-start space-x-2">
-          <a class="flex items-center justify-center p-2 bg-[#C5C1C0] text-black cursor-pointer rounded-md hover:opacity-50 ease-in duration-300 shadow-md">
+          <a
+            href={props.sourceSrc}
+            target="_blank"
+            rel="noreferrer"
+            class="flex items-center justify-center p-2 bg-[#C5C1C0] text-black cursor-pointer rounded-md hover:opacity-50 ease-in duration-300 shadow-md"
+          >
             <p>GitHub</p>
             <BsGithub class="ml-2" />
           </a>
-          <a class="flex items-center justify-center p-2 border-2 border-[#C5C1C0] cursor-pointer rounded-md hover:opacity-50 ease-in duration-300 shadow-md">
+          <a
+            href={props.liveSrc}
+            target="_blank"
+            rel="noreferrer"
+            class="flex items-center justify-center p-2 border-2 border-[#C5C1C0] cursor-pointer rounded-md hover:opacity-50 ease-in duration-300 shadow-md"
+          >
             <p>Live</p>
             <BsPostcard class="ml-2" />
           </a>
         </div>
         <div class="flex flex-col items-start justify-center text-base">
           <p>Created by --- Kyaw Thu</p>
-          <p>
-            Date --- 12<sup>th</sup> June 2023
-          </p>
-          <p>Category --- Hobby project</p>
+          <p>Date --- {props.date}</p>
+          <p>Category --- {props.category}</p>
         </div>
       </div>
       <HorizontalSeparator />
@@ -33,11 +55,9 @@ export default component$(() => {
           </div>
           <div class="flex flex-col items-start justify-start space-y-3 w-full mt-5 pl-5">
             <ul class="list-disc">
-              <li>Qwik</li>
-              <li>Qwik City</li>
-              <li>TypeScript</li>
-              <li>Tailwind CSS</li>
-              <li>Motion One</li>
+              {props.tech.map((text) => (
+                <li key={text}>{text}</li>
+              ))}
             </ul>
           </div>
         </section>
@@ -48,7 +68,9 @@ export default component$(() => {
           </div>
           <div class="flex flex-col items-start justify-start space-y-3 w-full mt-5 pl-5">
             <ul class="list-disc">
-              <li>Simple portfolio website</li>
+              {props.desc.map((text) => (
+                <li key={text}>{text}</li>
+              ))}
             </ul>
           </div>
         </section>
