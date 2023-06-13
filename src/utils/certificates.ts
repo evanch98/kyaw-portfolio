8eefdf62d93873ad4741f9cbce9874ccf821106d
Kyaw Thu <evanch98@gmail.com>
2023-06-13 22:47:53 +0800

    add a new object metaFrontEnd

---
+++ src/utils/certificates.ts
@@ -0,0 +1,23 @@
+export const metaFrontEnd = {
+  name: "Meta Front-End Professional Certificate",
+  platform: "Meta and Coursera",
+  date: "June 2023 - Present",
+  courses: [
+    "Introduction to Front-End Development",
+    "Programming with JavaScript",
+    "Version Control",
+    "HTML and CSS in depth",
+    "React Basics",
+    "Advanced React",
+    "Principles of UX/UI Design",
+    "Front-End Developer Capstone",
+    "Coding Interview Preparation",
+  ],
+  desc: [
+    "Create a responsive website using HTML to structure content, CSS to handle visual style, and JavaScript to develop interactive experiences",
+    "Learn to use React in relation to Javascript libraries and frameworks",
+    "Learn Bootstrap CSS Framework to create webpages and work with GitHub repositories and version control",
+    "Prepare for a coding interview, learn best approaches to problem-solving, and build portfolio-ready projects you can share during job interviews",
+  ],
+  verificationLink: "",
+};
