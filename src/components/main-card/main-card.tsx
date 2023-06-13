254f2a333fb008f749c00234e0dec9fda3a88115
Kyaw Thu <evanch98@gmail.com>
2023-06-13 12:00:39 +0800

    update the MainCard component

--- src/components/main-card/main-card.tsx
+++ src/components/main-card/main-card.tsx
@@ -11,6 +11,7 @@ import Resume from "../resume/resume";
 import Project from "../project/project";
 import Contact from "../contact/contact";
 import Link from "../link/link";
+import HorizontalSeparator from "../common/separators/horizontal-separator/horizontal-separator";
 
 export default component$(() => {
   const tab = useSignal("about");
@@ -41,58 +42,58 @@ export default component$(() => {
 
   return (
     <div class="flex-1 flex items-start">
-      <div class="flex h-[550px] flex-col items-center justify-evenly py-5 px-2 bg-[#E3DCD2] space-y-5 rounded-tl-md rounded-bl-md">
+      <div class="flex h-[550px] flex-col items-center justify-evenly py-5 px-2 bg-[#1A2930] space-y-5 rounded-tl-md rounded-bl-md drop-shadow-2xl">
         <div
           onClick$={() => (tab.value = "about")}
-          class={`flex flex-col items-center justify-center hover:text-[#CC8B65] ease-in duration-300 cursor-pointer ${
-            tab.value === "about" ? "text-[#CC8B65]" : "text-[#100C0D]"
+          class={`flex flex-col items-center justify-center hover:text-[#F7CE3E] ease-in duration-300 cursor-pointer ${
+            tab.value === "about" ? "text-[#F7CE3E]" : "text-[#C5C1C0]"
           }`}
         >
           <BsPerson class="w-[32px] h-auto" />
           <p class="tracking-widest text-sm">About</p>
         </div>
-        <hr class="w-full border-solid border-1 border-[#100C0D]" />
+        <HorizontalSeparator />
         <div
           onClick$={() => (tab.value = "resume")}
-          class={`flex flex-col items-center justify-center hover:text-[#CC8B65] ease-in duration-300 cursor-pointer ${
-            tab.value === "resume" ? "text-[#CC8B65]" : "text-[#100C0D]"
+          class={`flex flex-col items-center justify-center hover:text-[#F7CE3E] ease-in duration-300 cursor-pointer ${
+            tab.value === "resume" ? "text-[#F7CE3E]" : "text-[#C5C1C0]"
           }`}
         >
           <BsList class="w-[32px] h-auto" />
           <p class="tracking-widest text-sm">Resume</p>
         </div>
-        <hr class="w-full border-solid border-1 border-[#100C0D]" />
+        <HorizontalSeparator />
         <div
           onClick$={() => (tab.value = "projects")}
-          class={`flex flex-col items-center justify-center hover:text-[#CC8B65] ease-in duration-300 cursor-pointer ${
-            tab.value === "projects" ? "text-[#CC8B65]" : "text-[#100C0D]"
+          class={`flex flex-col items-center justify-center hover:text-[#F7CE3E] ease-in duration-300 cursor-pointer ${
+            tab.value === "projects" ? "text-[#F7CE3E]" : "text-[#C5C1C0]"
           }`}
         >
           <BsPersonWorkspace class="w-[32px] h-auto" />
           <p class="tracking-widest text-sm">Projects</p>
         </div>
-        <hr class="w-full border-solid border-1 border-[#100C0D]" />
+        <HorizontalSeparator />
         <div
           onClick$={() => (tab.value = "links")}
-          class={`flex flex-col items-center justify-center hover:text-[#CC8B65] ease-in duration-300 cursor-pointer ${
-            tab.value === "links" ? "text-[#CC8B65]" : "text-[#100C0D]"
+          class={`flex flex-col items-center justify-center hover:text-[#F7CE3E] ease-in duration-300 cursor-pointer ${
+            tab.value === "links" ? "text-[#F7CE3E]" : "text-[#C5C1C0]"
           }`}
         >
           <BsEye class="w-[32px] h-auto" />
           <p class="tracking-widest text-sm">Links</p>
         </div>
-        <hr class="w-full border-solid border-1 border-[#100C0D]" />
+        <HorizontalSeparator />
         <div
           onClick$={() => (tab.value = "contact")}
-          class={`flex flex-col items-center justify-center hover:text-[#CC8B65] ease-in duration-300 cursor-pointer ${
-            tab.value === "contact" ? "text-[#CC8B65]" : "text-[#100C0D]"
+          class={`flex flex-col items-center justify-center hover:text-[#F7CE3E] ease-in duration-300 cursor-pointer ${
+            tab.value === "contact" ? "text-[#F7CE3E]" : "text-[#C5C1C0]"
           }`}
         >
           <BsEnvelope class="w-[32px] h-auto" />
           <p class="tracking-widest text-sm">Contact</p>
         </div>
       </div>
-      <div class="w-full h-[550px] flex space-x-5 bg-[#013329] rounded-r-md overflow-auto p-5">
+      <div class="w-full h-[550px] flex space-x-5 bg-[#1A2930] rounded-r-md overflow-auto p-5">
         {bodyContent}
       </div>
     </div>
