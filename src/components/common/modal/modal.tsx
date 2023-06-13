7547b678b2d48289a641fc559efa37c17c1165d4
Kyaw Thu <evanch98@gmail.com>
2023-06-14 00:52:44 +0800

    update the Modal component

--- src/components/common/modal/modal.tsx
+++ src/components/common/modal/modal.tsx
@@ -1,8 +1,9 @@
 import type { QRL } from "@builder.io/qwik";
-import { Slot, component$ } from "@builder.io/qwik";
+import { Slot, component$, useVisibleTask$ } from "@builder.io/qwik";
 import Title from "../title/title";
 import HorizontalSeparator from "../separators/horizontal-separator/horizontal-separator";
 import { BsX } from "@qwikest/icons/bootstrap";
+import { animate } from "motion";
 
 interface ModalProps {
   title: string;
@@ -10,9 +11,15 @@ interface ModalProps {
 }
 
 export default component$((props: ModalProps) => {
+  useVisibleTask$(() => {
+    animate("#modal", { opacity: [0, 1] }, { easing: "ease-in", duration: 1 });
+  });
   return (
     <div class="absolute left-0 top-0 w-full h-full bg-black/70  z-50">
-      <div class="absolute w-auto max-h-[600px] top-20 left-1/2 -translate-x-1/2 p-5 bg-[#1A2930] text-[#C5C1C0] rounded-md shadow-xl overflow-y-auto">
+      <div
+        id="modal"
+        class="absolute w-auto max-h-[600px] top-20 left-1/2 -translate-x-1/2 p-5 bg-[#1A2930] text-[#C5C1C0] rounded-md shadow-xl overflow-y-auto"
+      >
         <div class="flex items-center justify-between">
           <Title title={props.title} />
           <BsX
