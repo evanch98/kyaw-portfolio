1037eef9840a88c32c3b203bf2f3282bf4446069
Kyaw Thu <evanch98@gmail.com>
2023-06-13 12:03:18 +0800

    update the about component

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -13,7 +13,7 @@ export default component$(() => {
   return (
     <div class="flex flex-col">
       <Title title="About" />
-      <div class="flex flex-col text-[#E3DCD2] text-lg mt-5 space-y-5">
+      <div class="flex flex-col text-[#C5C1C0] text-lg mt-5 space-y-5">
         <p>
           I have spent 7 years studying medicine at the University of Medicine
           1, Yangon. Due to several reasons, including pandemic, I had to drop
