dd1d9a51376ff981bde9e54eb43b74cfa9944281
Kyaw Thu <evanch98@gmail.com>
2023-06-14 01:28:39 +0800

    add degree.ts

---
+++ src/utils/degree.ts
@@ -0,0 +1,96 @@
+export const uopeopleCourses = [
+  {
+    code: "ENGL 0101",
+    course: "English Composition 1",
+    gpa: "3.67",
+  },
+  {
+    code: "CS 1101",
+    course: "Programming Fundamentals",
+    gpa: "4.0",
+  },
+  {
+    code: "UNIV 1001",
+    course: "Online Education Strategies",
+    gpa: "4.0",
+  },
+  {
+    code: "MATH 1201",
+    course: "College Algebra",
+    gpa: "4.0",
+  },
+  {
+    code: "CS 1102",
+    course: "Programming 1",
+    gpa: "4.0",
+  },
+  {
+    code: "ENGL 1102",
+    course: "English Composition 2",
+    gpa: "4.0",
+  },
+  {
+    code: "CS 2205",
+    course: "Web Programming 1",
+    gpa: "4.0",
+  },
+  {
+    code: "MATH 1302",
+    course: "Discrete Mathematics",
+    gpa: "4.0",
+  },
+  {
+    code: "CS 1104",
+    course: "Computer Systems",
+    gpa: "4.0",
+  },
+  {
+    code: "CS 3303",
+    course: "Data Structures",
+    gpa: "4.0",
+  },
+  {
+    code: "CS 2203",
+    course: "Databases 1",
+    gpa: "3.67",
+  },
+  {
+    code: "ENGL 1405",
+    course: "World Literature",
+    gpa: "4.0",
+  },
+  {
+    code: "CS 2204",
+    course: "Communications and Networking",
+    gpa: "4.0",
+  },
+  {
+    code: "MATH 1211",
+    course: "Calculus",
+    gpa: "4.0",
+  },
+  {
+    code: "CS 3308",
+    course: "Information Retrieval",
+    gpa: "4.0",
+  },
+];
+
+export const transferred = [
+  {
+    code: "SOPH 0027",
+    course: "Ancient Greek Philosopher",
+  },
+  {
+    code: "SOPH 0016",
+    course: "Environmental Science",
+  },
+  {
+    code: "SOPH 0020",
+    course: "Introduction to Ethics",
+  },
+  {
+    code: "SOPH 0043",
+    course: "Introduction to Web Development",
+  },
+];
