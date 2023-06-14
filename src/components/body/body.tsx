572c139183a4a900d82091ad1f5e40e5892ddf48
Kyaw Thu <evanch98@gmail.com>
2023-06-14 13:14:10 +0800

    update the Body component

--- src/components/body/body.tsx
+++ src/components/body/body.tsx
@@ -5,7 +5,7 @@ import MainCard from "../main-card/main-card";
 export default component$(() => {
   return (
     <>
-      <div class="xl:hidden flex flex-col text-[#C5C1C0] w-full p-5">
+      <div class="xl:hidden flex flex-col text-[#C5C1C0] w-full p-5 overflow-auto">
         <p>Mobile View</p>
       </div>
       <div class="hidden xl:flex justify-between items-center w-full text-[#C5C1C0] py-5 px-20 space-x-1">
