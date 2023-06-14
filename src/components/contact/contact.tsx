9adcade04b5bf46d3a3ff4e03b9990fe8ea74ab8
Kyaw Thu <evanch98@gmail.com>
2023-06-14 11:48:46 +0800

    update the Contact component

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,18 +1,31 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useVisibleTask$ } from "@builder.io/qwik";
 import Title from "../common/title/title";
 import HorizontalSeparator from "../common/separators/horizontal-separator/horizontal-separator";
 import { BsSend } from "@qwikest/icons/bootstrap";
+import { animate, spring, stagger } from "motion";
 
 export default component$(() => {
+  useVisibleTask$(() => {
+    animate("#contact", { opacity: [0, 1] }, { easing: "ease-in", duration: 1 });
+    animate(
+      ".info",
+      { x: [-1000, 0] },
+      { easing: spring({ velocity: 50, stiffness: 50 }), delay: stagger(0.15) }
+    );
+  });
   return (
-    <div class="flex flex-col w-full text-[#F7CE3EC5C1C0]">
+    <div id="contact" class="flex flex-col w-full text-[#F7CE3EC5C1C0]">
       <Title title="Get in Touch" />
       <div class="flex flex-col mt-5 space-y-2 text-lg">
-        <p>Name --- Kyaw Thu</p>
-        <p>Email --- evanch98@gmail.com || kyawthuch98@gmail.com</p>
-        <p>Phone --- +886 958 437 425</p>
-        <p>Address --- New Taipei City, Taiwan</p>
-        <p>Availability --- Full-time, part-time, internship, and freelance</p>
+        <p class="info">Name --- Kyaw Thu</p>
+        <p class="info">
+          Email --- evanch98@gmail.com || kyawthuch98@gmail.com
+        </p>
+        <p class="info">Phone --- +886 958 437 425</p>
+        <p class="info">Address --- New Taipei City, Taiwan</p>
+        <p class="info">
+          Availability --- Full-time, part-time, internship, and freelance
+        </p>
       </div>
       <HorizontalSeparator />
       <Title title="Contact Form" />
