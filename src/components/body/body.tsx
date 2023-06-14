34363907935207099bb1206c728081544f829fa9
Kyaw Thu <evanch98@gmail.com>
2023-06-14 13:53:00 +0800

    update the MobileAboutCard component

--- src/components/body/body.tsx
+++ src/components/body/body.tsx
@@ -3,12 +3,13 @@ import IntroCard from "../intro-card/intro-card";
 import MainCard from "../main-card/main-card";
 import MobileIntroCard from "../mobile/mobile-intro-card/mobile-intro-card";
 import MobileAboutCard from "../mobile/mobile-about-card/mobile-about-card";
+import { IoAppsOutline } from "@qwikest/icons/ionicons";
 
 export default component$(() => {
   return (
     <>
-      <div class="xl:hidden flex flex-col items-center justify-center text-[#C5C1C0] w-full p-2 overflow-auto">
-        <div class="mb-2 w-full text-right">Menu</div>
+      <div class="xl:hidden flex flex-col items-center justify-end text-[#C5C1C0] w-full p-2 overflow-auto">
+        <IoAppsOutline class="w-[28px] h-auto self-end mb-2" />
         <MobileIntroCard />
         <MobileAboutCard />
       </div>
