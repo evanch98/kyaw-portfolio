16d9222dbf0a459b7be07ce117fc7b6e69a98e39
Kyaw Thu <evanch98@gmail.com>
2023-06-14 12:27:44 +0800

    add useMediaQuery.ts

---
+++ src/hook/useMediaQuery.ts
@@ -0,0 +1,41 @@
+import { $, useTask$, useSignal } from "@builder.io/qwik";
+
+export function useMediaQuery(query: string): boolean {
+  const getMatches = (query: string): boolean => {
+    // Prevents SSR issues
+    if (typeof window !== "undefined") {
+      return window.matchMedia(query).matches;
+    }
+    return false;
+  };
+
+  const matches = useSignal<boolean>(getMatches(query));
+
+  const handleChange = $(function handleChange() {
+    matches.value = getMatches(query);
+  });
+
+  useTask$(() => {
+    const matchMedia = window.matchMedia(query);
+
+    // Triggered at the first client-side load and if query changes
+    handleChange();
+
+    // Listen matchMedia
+    if (matchMedia.addListener) {
+      matchMedia.addListener(handleChange);
+    } else {
+      matchMedia.addEventListener("change", handleChange);
+    }
+
+    return () => {
+      if (matchMedia.removeListener) {
+        matchMedia.removeListener(handleChange);
+      } else {
+        matchMedia.removeEventListener("change", handleChange);
+      }
+    };
+  });
+
+  return matches.value;
+}
