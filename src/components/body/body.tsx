b1ddfa9c326764fbf5ee1af9020264c82960db1d
Kyaw Thu <evanch98@gmail.com>
2023-06-14 13:38:03 +0800

    update the Body component

--- src/components/body/body.tsx
+++ src/components/body/body.tsx
@@ -2,6 +2,7 @@ import { component$ } from "@builder.io/qwik";
 import IntroCard from "../intro-card/intro-card";
 import MainCard from "../main-card/main-card";
 import MobileIntroCard from "../mobile/mobile-intro-card/mobile-intro-card";
+import MobileAboutCard from "../mobile/mobile-about-card/mobile-about-card";
 
 export default component$(() => {
   return (
@@ -9,6 +10,7 @@ export default component$(() => {
       <div class="xl:hidden flex flex-col items-center justify-center text-[#C5C1C0] w-full p-2 overflow-auto">
         <div class="mb-2 w-full text-right">Menu</div>
         <MobileIntroCard />
+        <MobileAboutCard />
       </div>
       <div class="hidden xl:flex justify-between items-center w-full text-[#C5C1C0] py-5 px-20 space-x-1">
         <IntroCard />
