6633be454b5574539d0deae62db2878e102d2c78
Kyaw Thu <evanch98@gmail.com>
2023-06-14 17:46:16 +0800

    update the Body component

--- src/components/body/body.tsx
+++ src/components/body/body.tsx
@@ -3,16 +3,48 @@ import IntroCard from "../intro-card/intro-card";
 import MainCard from "../main-card/main-card";
 import MobileIntroCard from "../mobile/mobile-intro-card/mobile-intro-card";
 import MobileAboutCard from "../mobile/mobile-about-card/mobile-about-card";
-import { BsGrid3X3GapFill } from "@qwikest/icons/bootstrap";
+import {
+  BsGrid3X3GapFill,
+  BsList,
+  BsPersonWorkspace,
+  BsEye,
+  BsEnvelope,
+} from "@qwikest/icons/bootstrap";
 
 export default component$(() => {
   const sideMenu = useSignal(false);
   return (
     <>
-      <div class="xl:hidden flex flex-col items-center justify-end text-[#C5C1C0] w-full p-2 h-full">
+      <div class="xl:hidden flex flex-col items-center justify-start text-[#C5C1C0] w-full p-2 h-full relative">
+        <div
+          class={`${
+            sideMenu.value
+              ? "fixed left-0 top-0 w-full h-screen bg-[#1A2930] p-10 ease-in duration-500 z-50 flex flex-col items-center justify-center"
+              : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+          }`}
+        >
+          <div class="flex flex-col items-center justify-start space-y-5">
+            <div class="text-xl flex items-center w-full justify-start space-x-1">
+              <BsList />
+              <p>Resume</p>
+            </div>
+            <div class="text-xl flex items-center w-full justify-start space-x-1">
+              <BsPersonWorkspace />
+              <p>Projects</p>
+            </div>
+            <div class="text-xl flex items-center w-full justify-start space-x-1">
+              <BsEye />
+              <p>Links</p>
+            </div>
+            <div class="text-xl flex items-center w-full justify-start space-x-1">
+              <BsEnvelope />
+              <p>Contact</p>
+            </div>
+          </div>
+        </div>
         <BsGrid3X3GapFill
           onClick$={() => (sideMenu.value = true)}
-          class="w-[28px] h-auto self-end mb-2"
+          class="w-[28px] h-auto self-start mb-2"
         />
         <MobileIntroCard />
         <MobileAboutCard />
