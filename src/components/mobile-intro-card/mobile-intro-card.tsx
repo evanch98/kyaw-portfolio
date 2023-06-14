d72a8ae30a0bd028242b563a5911c46c7888ff27
Kyaw Thu <evanch98@gmail.com>
2023-06-14 13:27:35 +0800

    add a new component MobileIntroCard

---
+++ src/components/mobile-intro-card/mobile-intro-card.tsx
@@ -0,0 +1,9 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+  return (
+    <div class="w-full p-2 bg-[#1A2930] rounded-md">
+      <p>Mobile</p>
+    </div>
+  );
+});
