cc3b44b46709fe29188a08e7dade32df02f8a3a7
Kyaw Thu <evanch98@gmail.com>
2023-06-16 16:12:46 +0800

    update the Project component

--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -1,4 +1,10 @@
-import { $, component$, useSignal, useStore, useVisibleTask$ } from "@builder.io/qwik";
+import {
+  $,
+  component$,
+  useSignal,
+  useStore,
+  useVisibleTask$,
+} from "@builder.io/qwik";
 import Title from "../common/title/title";
 import { Image } from "@unpic/qwik";
 import VerticalSeparator from "../common/separators/vertical-separator/vertical-separator";
@@ -10,10 +16,15 @@ import ProjectTemplate from "../common/project-template/project-template";
 import AllProjects from "../all-projects/all-projects";
 import { animate } from "motion";
 import type { projectModalStore, projectObject } from "~/types/types";
+import ProjectBox from "../common/project-box/project-box";
 
 export default component$(() => {
   useVisibleTask$(() => {
-    animate("#project", { opacity: [0, 1] }, { easing: "ease-in", duration: 1 });
+    animate(
+      "#project",
+      { opacity: [0, 1] },
+      { easing: "ease-in", duration: 1 }
+    );
   });
   const projectModal: projectModalStore = useStore({
     open: false,
@@ -25,7 +36,7 @@ export default component$(() => {
       category: "",
       tech: [""],
       desc: [""],
-    } as projectObject ,
+    } as projectObject,
   } as projectModalStore);
   const allProjectsModal = useSignal(false);
   const onAllProjectsModalClose = $(() => {
@@ -57,82 +68,33 @@ export default component$(() => {
         </Modal>
       )}
       <Title title="Projects" />
+      {/* Projects section START */}
       <div class="flex flex-col mt-5 space-y-6">
         <div class="flex items-start justify-between">
           {/* Project 1 */}
-          <div class="w-full flex flex-col items-center justify-center space-y-2">
-            <div
-              onClick$={() => {
-                projectModal.open = true;
-                projectModal.projectData = { ...project1 };
-              }}
-              class="relative group"
-            >
-              <Image
-                src="/projects/airbnb.png"
-                width={300}
-                height={148}
-                alt="airbnb"
-                layout="constrained"
-                class="group-hover:opacity-50 cursor-pointer ease-in duration-300"
-              />
-              <div class="absolute opacity-0 group-hover:opacity-100 ease-in duration-300 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-black">
-                <BsPlusCircle class="w-[52px] h-auto cursor-pointer" />
-              </div>
-            </div>
-            <p class="text-lg font-semibold">{project1.title}</p>
-            <p class="text-sm">{project1.tech[0]}</p>
-          </div>
+          <ProjectBox
+            projectModal={projectModal}
+            project={project1}
+            imgSrc="/projects/airbnb.png"
+            imgAlt="airbnb"
+          />
           <VerticalSeparator />
           {/* Project 2 */}
-          <div class="w-full flex flex-col items-center justify-center space-y-2">
-            <div
-              onClick$={() => {
-                projectModal.open = true;
-                projectModal.projectData = { ...project2 };
-              }}
-              class="relative group"
-            >
-              <Image
-                src="/projects/share_prompts.png"
-                width={300}
-                height={148}
-                alt="airbnb"
-                layout="constrained"
-                class="group-hover:opacity-70 cursor-pointer ease-in duration-300"
-              />
-              <div class="absolute opacity-0 group-hover:opacity-100 ease-in duration-300 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-black">
-                <BsPlusCircle class="w-[52px] h-auto cursor-pointer" />
-              </div>
-            </div>
-            <p class="text-lg font-semibold">{project2.title}</p>
-            <p class="text-sm">{project2.tech[0]}</p>
-          </div>
+          <ProjectBox
+            projectModal={projectModal}
+            project={project2}
+            imgSrc="/projects/share_prompts.png"
+            imgAlt="share prompts"
+          />
         </div>
         <div class="flex items-start justify-between">
-          <div class="w-full flex flex-col items-center justify-center space-y-2">
-            <div
-              onClick$={() => {
-                projectModal.open = true;
-                projectModal.projectData = { ...project3 };
-              }}
-              class="relative group"
-            >
-              <Image
-                src="/projects/airbnb.png"
-                width={300}
-                height={148}
-                alt="airbnb"
-                layout="constrained"
-                class="group-hover:opacity-50 cursor-pointer ease-in duration-300"
-              />
-              <div class="absolute opacity-0 group-hover:opacity-100 ease-in duration-300 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-black">
-                <BsPlusCircle class="w-[52px] h-auto" />
-              </div>
-            </div>
-            <p class="text-lg font-semibold">{project3.title}</p>
-            <p class="text-sm">{project3.tech[0]}</p>
-          </div>
+          {/* Project 3 */}
+          <ProjectBox
+            projectModal={projectModal}
+            project={project3}
+            imgSrc="/projects/share_prompts.png"
+            imgAlt="furry coats"
+          />
           <VerticalSeparator />
           <div class="w-full flex flex-col items-center justify-center space-y-2">
             <div class="relative group">
@@ -153,6 +115,7 @@ export default component$(() => {
           </div>
         </div>
       </div>
+      {/* Projects section END */}
       <HorizontalSeparator />
       <p
         onClick$={() => {
