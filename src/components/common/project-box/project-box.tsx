89323b5a8606672f109dd6741a8bf575db4372c9
Kyaw Thu <evanch98@gmail.com>
2023-06-16 16:02:04 +0800

    update the ProjectBox

--- src/components/common/project-box/project-box.tsx
+++ src/components/common/project-box/project-box.tsx
@@ -1,5 +1,37 @@
 import { component$ } from "@builder.io/qwik";
+import { BsPlusCircle } from "@qwikest/icons/bootstrap";
+import { Image } from "@unpic/qwik";
+import type { projectModalStore, projectObject } from "~/types/types";
 
-export default component$(() => {
-  return <div></div>
+interface ProjectBoxProps {
+  projectModal: projectModalStore;
+  project: projectObject;
+}
+
+export default component$((props: ProjectBoxProps) => {
+  return (
+    <div class="w-full flex flex-col items-center justify-center space-y-2">
+      <div
+        onClick$={() => {
+          props.projectModal.open = true;
+          props.projectModal.projectData = { ...props.project };
+        }}
+        class="relative group"
+      >
+        <Image
+          src="/projects/airbnb.png"
+          width={300}
+          height={148}
+          alt="airbnb"
+          layout="constrained"
+          class="group-hover:opacity-50 cursor-pointer ease-in duration-300"
+        />
+        <div class="absolute opacity-0 group-hover:opacity-100 ease-in duration-300 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-black">
+          <BsPlusCircle class="w-[52px] h-auto cursor-pointer" />
+        </div>
+      </div>
+      <p class="text-lg font-semibold">{props.project.title}</p>
+      <p class="text-sm">{props.project.tech[0]}</p>
+    </div>
+  );
 });
