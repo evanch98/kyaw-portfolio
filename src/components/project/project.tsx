2d15a1f191c1302e903778db05cd396ce3dedea7
Kyaw Thu <evanch98@gmail.com>
2023-06-16 17:47:26 +0800

    update the Project component

--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -6,11 +6,9 @@ import {
   useVisibleTask$,
 } from "@builder.io/qwik";
 import Title from "../common/title/title";
-import { Image } from "@unpic/qwik";
 import VerticalSeparator from "../common/separators/vertical-separator/vertical-separator";
-import { BsPlusCircle } from "@qwikest/icons/bootstrap";
 import HorizontalSeparator from "../common/separators/horizontal-separator/horizontal-separator";
-import { project1, project2, project3 } from "~/utils/projects";
+import { project1, project2, project3, project4 } from "~/utils/projects";
 import Modal from "../common/modal/modal";
 import ProjectTemplate from "../common/project-template/project-template";
 import AllProjects from "../all-projects/all-projects";
@@ -86,23 +84,12 @@ export default component$(() => {
           />
           <VerticalSeparator />
           {/* Project 4 */}
-          <div class="w-full flex flex-col items-center justify-center space-y-2">
-            <div class="relative group">
-              <Image
-                src="/projects/share_prompts.png"
-                width={300}
-                height={148}
-                alt="airbnb"
-                layout="constrained"
-                class="group-hover:opacity-70 cursor-pointer ease-in duration-300"
-              />
-              <div class="absolute opacity-0 group-hover:opacity-100 ease-in duration-300 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-black">
-                <BsPlusCircle class="w-[52px] h-auto" />
-              </div>
-            </div>
-            <p class="text-lg font-semibold">Share Prompts</p>
-            <p class="text-sm">Web Application</p>
-          </div>
+          <ProjectBox
+            projectModal={projectModal}
+            project={project4}
+            imgSrc="/projects/airbnb.png"
+            imgAlt="spotify"
+          />
         </div>
       </div>
       {/* Projects section END */}
