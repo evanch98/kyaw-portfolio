039494d89079de2710884b9070ef96f101f38031
Kyaw Thu <evanch98@gmail.com>
2023-06-19 00:56:57 +0800

    add a new component VirtualExperience

---
+++ src/components/resume/virtual-experience/virtual-experience.tsx
@@ -0,0 +1,20 @@
+import { component$ } from "@builder.io/qwik";
+
+interface VirtualExperienceProps {
+  title: string;
+  company: string;
+  date: string;
+  url: string;
+}
+
+export default component$((props: VirtualExperienceProps) => {
+  return (
+    <div class="mt-5">
+      <a href={props.url} class="cursor-pointer hover:text-[#F7CE3E] ease-in duration-300">
+        {props.title}
+      </a>
+      <p>{props.company}</p>
+      <p>{props.date}</p>
+    </div>
+  );
+});
