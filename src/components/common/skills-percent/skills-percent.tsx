63c80c5771e751cd5326d8437dd8eb28994e67e2
Kyaw Thu <evanch98@gmail.com>
2023-06-19 01:26:56 +0800

    update the SkillsPercent component

--- src/components/common/skills-percent/skills-percent.tsx
+++ src/components/common/skills-percent/skills-percent.tsx
@@ -1,5 +1,19 @@
 import { component$ } from "@builder.io/qwik";
 
-export default component$(() => {
-  return <div></div>;
+interface SkillsPercentProps {
+  skill: string;
+  percent: string;
+}
+
+export default component$((props: SkillsPercentProps) => {
+  return (
+    <div class="flex flex-col w-full space-y-1">
+      <p>{props.skill}</p>
+      <div class="h-[5.5px] w-full bg-[#C5C1C0] rounded-lg overflow-hidden">
+        <div
+          class={`h-[5.5px] bg-[#F7CE3E]" style="width: ${props.percent}`}
+        ></div>
+      </div>
+    </div>
+  );
 });
