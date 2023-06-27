7551216bde0b71cfa7bc24d26c5c81b12514d09a
Kyaw Thu <evanch98@gmail.com>
2023-06-28 00:53:45 +0800

    update the index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
-import Body from '~/components/body/body';
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+import Body from "~/components/body/body";
 
 export default component$(() => {
   return (
@@ -11,11 +11,12 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: 'Kyaw | Front-End Developer',
+  title: "Kyaw | Front-End Developer",
   meta: [
     {
-      name: 'Kyaw Thu\'s Portfolio Website',
-      content: 'This website is designed and built by Kyaw Thu. It is built by using Qwik, Qwik City, TypeScript, Motion One, and Tailwind CSS.',
+      name: "description",
+      content:
+        "This website is designed and built by Kyaw Thu. It is built by using Qwik, Qwik City, TypeScript, Motion One, and Tailwind CSS.",
     },
   ],
 };
