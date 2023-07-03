9c43ff18a8ffd069ba754189bc1381ee62232bb8
Kyaw Thu <evanch98@gmail.com>
2023-07-03 14:26:34 +0800

    configuration

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,10 @@
 import { Parameters } from 'storybook-framework-qwik';
 
+import { withThemeByClassName } from '@storybook/addon-styling';
+
+/* TODO: update import to your tailwind styles file. If you're using Angular, inject this through your angular.json config instead */
+import '../src/global.css';
+
 export const parameters: Parameters = {
   a11y: {
     config: {},
@@ -15,3 +20,15 @@ export const parameters: Parameters = {
     iframeHeight: '200px',
   },
 };
+
+export const decorators = [
+  // Adds theme switching support.
+  // NOTE: requires setting "darkMode" to "class" in your tailwind config
+  withThemeByClassName({
+    themes: {
+      light: 'light',
+      dark: 'dark',
+    },
+    defaultTheme: 'light',
+  }),
+];
