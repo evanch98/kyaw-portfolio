df4605d3c960e68a95352efa99e1c3e14d44be62
Kyaw Thu <evanch98@gmail.com>
2023-07-05 18:47:39 +0800

    add allProjects.ts

---
+++ src/utils/allProjects.ts
@@ -0,0 +1,62 @@
+export const allProjects = [
+  {
+    title: "Vue Todos (Vue)",
+    type: "web",
+    sourceUrl: "https://github.com/evanch98/todo-app-vue",
+    category: "Hobby Project",
+  },
+  {
+    title: "Simple Calculator (Android/Kotlin)",
+    type: "mobile",
+    sourceUrl: "https://github.com/evanch98/calculator-android-kotlin",
+    category: "Undergraduate Project",
+  },
+  {
+    title: "BMI Calculator (Android/Kotlin)",
+    type: "mobile",
+    sourceUrl: "https://github.com/evanch98/bmi-android-kotlin",
+    category: "Undergraduate Project",
+  },
+  {
+    title: "Spotify Clone (NextJS)",
+    type: "web",
+    sourceUrl: "https://github.com/evanch98/spotify-clone-nextjs",
+    category: "Hobby Project",
+  },
+  {
+    title: "Chess (JavaScript)",
+    type: "web",
+    sourceUrl: "https://github.com/evanch98/chess-javascript",
+    category: "Hobby Project",
+  },
+  {
+    title: "My Portfolio Website (Qwik)",
+    type: "web",
+    sourceUrl: "https://github.com/evanch98/kyaw-portfolio",
+    category: "Hobby Project",
+  },
+  {
+    title: "Share Prompts (NextJS)",
+    type: "web",
+    sourceUrl: "https://github.com/evanch98/share-prompts-nextjs",
+    category: "Hobby Project",
+  },
+  {
+    title: "Airbnb Clone (NextJS)",
+    type: "web",
+    sourceUrl: "https://github.com/evanch98/airbnb_clone_react",
+    category: "Hobby Project",
+  },
+  {
+    title: "Twitter Clone (Flutter)",
+    type: "mobile",
+    sourceUrl: "https://github.com/evanch98/twitter_clone",
+    category: "Hobby Project",
+  },
+  {
+    title: "Little Lemon (Android/Kotlin)",
+    type: "mobile",
+    sourceUrl: "https://github.com/evanch98/little-lemon",
+    category: "Capstone Project",
+  },
+];
