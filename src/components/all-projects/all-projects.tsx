5b0293a5f2402f09357023a93d6f64c1864017a8
Kyaw Thu <evanch98@gmail.com>
2023-07-05 18:48:45 +0800

    update the AllProjects component

--- src/components/all-projects/all-projects.tsx
+++ src/components/all-projects/all-projects.tsx
@@ -1,63 +1,9 @@
 import { component$ } from "@builder.io/qwik";
 import { IoLogoReact, IoPhonePortraitOutline } from "@qwikest/icons/ionicons";
 import { BsGithub } from "@qwikest/icons/bootstrap";
+import { allProjects } from "~/utils/allProjects";
 
-const projects = [
-  {
-    title: "Simple Calculator (Android/Kotlin)",
-    type: "mobile",
-    sourceUrl: "https://github.com/evanch98/calculator-android-kotlin",
-    category: "Undergraduate Project",
-  },
-  {
-    title: "BMI Calculator (Android/Kotlin)",
-    type: "mobile",
-    sourceUrl: "https://github.com/evanch98/bmi-android-kotlin",
-    category: "Undergraduate Project",
-  },
-  {
-    title: "Spotify Clone (NextJS)",
-    type: "web",
-    sourceUrl: "https://github.com/evanch98/spotify-clone-nextjs",
-    category: "Hobby Project",
-  },
-  {
-    title: "Chess (JavaScript)",
-    type: "web",
-    sourceUrl: "https://github.com/evanch98/chess-javascript",
-    category: "Hobby Project",
-  },
-  {
-    title: "My Portfolio Website (Qwik)",
-    type: "web",
-    sourceUrl: "https://github.com/evanch98/kyaw-portfolio",
-    category: "Hobby Project",
-  },
-  {
-    title: "Share Prompts (NextJS)",
-    type: "web",
-    sourceUrl: "https://github.com/evanch98/share-prompts-nextjs",
-    category: "Hobby Project",
-  },
-  {
-    title: "Airbnb Clone (NextJS)",
-    type: "web",
-    sourceUrl: "https://github.com/evanch98/airbnb_clone_react",
-    category: "Hobby Project",
-  },
-  {
-    title: "Twitter Clone (Flutter)",
-    type: "mobile",
-    sourceUrl: "https://github.com/evanch98/twitter_clone",
-    category: "Hobby Project",
-  },
-  {
-    title: "Little Lemon (Android/Kotlin)",
-    type: "mobile",
-    sourceUrl: "https://github.com/evanch98/little-lemon",
-    category: "Capstone Project",
-  },
-];
+const projects = allProjects;
 
 export default component$(() => {
   return (
