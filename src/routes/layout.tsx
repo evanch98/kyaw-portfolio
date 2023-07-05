749215db0abef9e99b00a0f7dbc71c0b7b417ff0
Kyaw Thu <evanch98@gmail.com>
2023-07-05 18:54:21 +0800

    update the layout.tsx

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -23,7 +23,7 @@ export const useServerTimeLoader = routeLoader$(() => {
 
 export default component$(() => {
   return (
-    <div class='flex flex-col h-full relative'>
+    <div class='flex flex-col h-full lg:h-screen relative'>
       <main class="flex-1 flex flex-col">
         <Header />
         <Slot />
