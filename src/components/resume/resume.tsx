b042cfb64dc897076963b53c2bc52db4e31aab28
Kyaw Thu <evanch98@gmail.com>
2023-07-10 00:55:34 +0800

    refator the code

--- src/components/resume/resume.tsx
+++ src/components/resume/resume.tsx
@@ -30,6 +30,7 @@ import VirtualExperience from "./virtual-experience/virtual-experience";
 import CertificateBox from "../common/certificate-box/certificate-box";
 import SkillsPercent from "../common/skills-percent/skills-percent";
 import AllCertificates from "./all-certificates/all-certificates";
+import Skills from "./Skills";
 
 export default component$(() => {
   useVisibleTask$(() => {
@@ -51,6 +52,10 @@ export default component$(() => {
   const onUopeopleModalClose = $(() => {
     uopeopleModal.value = false;
   });
+
+  const onFrontEndModalOpen = $(() => {
+    frontEndModal.value = true
+  })
 
   const onFrontEndModalClose = $(() => {
     frontEndModal.value = false;
@@ -127,69 +132,7 @@ export default component$(() => {
       </div>
       <HorizontalSeparator />
       <Title title="Skills" />
-      <div class="flex flex-col mt-5 space-y-6">
-        <div class="flex flex-col lg:flex-row items-start justify-between">
-          <section class="w-full flex flex-col items-center justify-center">
-            <div
-              onClick$={() => (frontEndModal.value = true)}
-              class="flex justify-center items-center space-x-2 cursor-pointer hover:text-[#F7CE3E] ease-in duration-300"
-            >
-              <h1 class="text-xl font-bold">Front-End</h1>
-              <IoLogoReact class="w-[24px] h-auto" />
-            </div>
-            <div class="flex flex-col items-start justify-start space-y-3 w-full mt-5 px-20">
-              <SkillsPercent skill="Next JS" percent="70%" />
-              <SkillsPercent skill="Qwik" percent="80%" />
-              <SkillsPercent skill="Tailwind CSS" percent="80%" />
-              <SkillsPercent skill="TypeScript" percent="75%" />
-            </div>
-          </section>
-          <HorizontalSeparator class="lg:hidden w-10 self-center" />
-          <VerticalSeparator class="hidden lg:flex" />
-          <section class="w-full flex flex-col items-center justify-center">
-            <div class="flex justify-center items-center space-x-2">
-              <h1 class="text-xl font-bold">Mobile</h1>
-              <IoPhonePortraitOutline class="w-[24px] h-auto" />
-            </div>
-            <div class="flex flex-col items-start justify-start space-y-3 w-full mt-5 px-20">
-              <SkillsPercent skill="Dart" percent="70%" />
-              <SkillsPercent skill="Flutter" percent="70%" />
-              <SkillsPercent skill="Kotlin" percent="60%" />
-              <SkillsPercent skill="Jetpack Compose" percent="50%" />
-            </div>
-          </section>
-        </div>
-        <HorizontalSeparator class="w-10 self-center" />
-        <div class="flex flex-col lg:flex-row items-start justify-between">
-          <section class="w-full flex flex-col items-center justify-center">
-            <div class="flex justify-center items-center space-x-2">
-              <h1 class="text-xl font-bold">Languages</h1>
-              <IoChatboxOutline class="w-[24px] h-auto" />
-            </div>
-            <div class="flex flex-col items-start justify-start space-y-3 w-full mt-5 px-20">
-              <SkillsPercent skill="Burmese" percent="100%" />
-              <SkillsPercent skill="English" percent="85%" />
-              <SkillsPercent skill="Traditional Chinese" percent="35%" />
-            </div>
-          </section>
-          <HorizontalSeparator class="lg:hidden w-10 self-center" />
-          <VerticalSeparator class="hidden lg:flex" />
-          <section class="w-full flex flex-col items-center justify-center">
-            <div class="flex justify-center items-center space-x-2">
-              <h1 class="text-xl font-bold">Soft Skills</h1>
-              <IoGlobeOutline class="w-[24px] h-auto" />
-            </div>
-            <div class="flex flex-col items-start justify-start space-y-3 w-full mt-5 px-20">
-              <ul class="list-disc">
-                <li>Strong problem solving and analytic skills</li>
-                <li>Ability to learn new technologies quickly</li>
-                <li>Good communication skills</li>
-                <li>Willingness to explore and learn</li>
-              </ul>
-            </div>
-          </section>
-        </div>
-      </div>
+      <Skills frontEndOnClick={onFrontEndModalOpen} />
       <HorizontalSeparator />
       <Title title="Virtual Experience" />
       <div class="flex flex-col space-y-2 pb-2">
