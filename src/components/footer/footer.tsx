d84eeabb880787e1b6204182b07963320efcd18f
Kyaw Thu <evanch98@gmail.com>
2023-07-10 01:31:27 +0800

    update the footer component

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,7 +4,7 @@ export default component$(() => {
   return (
     <footer class="flex items-center justify-center bg-[#1A2930] p-2">
       <p class="text-[#C5C1C0] text-sm">
-        Designed and built by Kyaw Thu using Qwik, 2023 ⚡
+        Designed and built by Kyaw Thu using Qwik &copy; {new Date().getFullYear().toString()} | All rights reserved ⚡
       </p>
     </footer>
   );
