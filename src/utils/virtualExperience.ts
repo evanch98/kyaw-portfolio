bf4b9ad99440138e36cd2ae96d9c3125ca46f841
Kyaw Thu <evanch98@gmail.com>
2023-07-10 12:57:46 +0800

    update the virtualExperience.ts file

---
+++ src/utils/virtualExperience.ts
@@ -0,0 +1,24 @@
+import type { virtualExperienceObject } from "~/types/types";
+
+const virtualExperienceList: virtualExperienceObject[] = [
+  {
+    title: "Front-End Software Engineering Virtual Experience Programme",
+    company: "Skyscanner and Forage",
+    date: "June 18th, 2023",
+    url: "https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/Skyscanner/km4rw7dihDr3etqom_Skyscanner_j5vFHRiaXbcY3ndzS_1687106629061_completion_certificate.pdf",
+  },
+  {
+    title: "Mobile Engineering Virtual Experience Program",
+    company: "Lyft and Forage",
+    date: "January 10th, 2023",
+    url: "https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/Lyft/z5WMH5vehnwDferzi_Lyft_j5vFHRiaXbcY3ndzS_1673407301433_completion_certificate.pdf",
+  },
+  {
+    title: "Developer Program",
+    company: "Accenture Nordics and Forage",
+    date: "March 22nd, 2022",
+    url: "https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/Accenture%20Nordics/PxenP4rHNE6Bh4nQz_Accenture%20Nordics_j5vFHRiaXbcY3ndzS_1673407291869_completion_certificate.pdf",
+  },
+];
+
+export default virtualExperienceList;
