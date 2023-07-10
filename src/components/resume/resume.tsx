ca349a00eb201b7d9a070c96082bb5a4a64bf239
Kyaw Thu <evanch98@gmail.com>
2023-07-10 13:04:02 +0800

    update the Resume component

--- src/components/resume/resume.tsx
+++ src/components/resume/resume.tsx
@@ -113,7 +113,7 @@ export default component$(() => {
           />
         </Modal>
       )}
-      
+
       {/* Education section - START */}
       <Title title="Education" />
       <Education
@@ -128,27 +128,10 @@ export default component$(() => {
       <Skills frontEndOnClick={onFrontEndModalOpen} />
       {/* Skills section - END */}
       <HorizontalSeparator />
+      {/* Virtual Experience - START */}
       <Title title="Virtual Experience" />
-      <div class="flex flex-col space-y-2 pb-2">
-        <VirtualExperience
-          title="Front-End Software Engineering Virtual Experience Programme"
-          company="Skyscanner and Forage"
-          date="June 18th, 2023"
-          url="https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/Skyscanner/km4rw7dihDr3etqom_Skyscanner_j5vFHRiaXbcY3ndzS_1687106629061_completion_certificate.pdf"
-        />
-        <VirtualExperience
-          title="Mobile Engineering Virtual Experience Program"
-          company="Lyft and Forage"
-          date="January 10th, 2023"
-          url="https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/Lyft/z5WMH5vehnwDferzi_Lyft_j5vFHRiaXbcY3ndzS_1673407301433_completion_certificate.pdf"
-        />
-        <VirtualExperience
-          title="Developer Program"
-          company="Accenture Nordics and Forage"
-          date="March 22nd, 2022"
-          url="https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/Accenture%20Nordics/PxenP4rHNE6Bh4nQz_Accenture%20Nordics_j5vFHRiaXbcY3ndzS_1673407291869_completion_certificate.pdf"
-        />
-      </div>
+      <VirtualExperience />
+      {/* Virtual Experience - END */}
     </div>
   );
 });
