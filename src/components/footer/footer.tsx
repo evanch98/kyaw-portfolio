97b68f9d4109993b5d646b02abe794f7505d0b4b
Kyaw Thu <evanch98@gmail.com>
2023-07-12 00:52:57 +0800

    hide the footer on mobile screen

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,7 +2,7 @@ import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
-    <footer class="flex items-center justify-center bg-[#1A2930] p-2">
+    <footer class="hidden xl:flex items-center justify-center bg-[#1A2930] p-2">
       <p class="text-[#C5C1C0] text-sm text-center">
         Designed and built by Kyaw Thu using Qwik &copy; {new Date().getFullYear().toString()} | All rights reserved ⚡
       </p>
